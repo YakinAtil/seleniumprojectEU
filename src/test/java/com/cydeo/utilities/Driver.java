b14e01b854920d59a237987b0c196bf82e8937d5
Atil Yakin <atlykn@gmail.com>
2022-06-04 14:58:07 +0300

    Web Driver Utility

---
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -0,0 +1,16 @@
+package com.cydeo.utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class Driver {
+
+    private Driver(){}
+
+    //Close access outside class; static because static method.
+
+    private static WebDriver driver;
+
+
+
+
+}
