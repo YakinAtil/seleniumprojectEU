49b720f9690e45487cd815720f8903f00d7de7e2
Atil Yakin <atlykn@gmail.com>
2022-06-05 16:19:26 +0300

    Upload practice finished

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T02_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T02_Upload_Practice.java
@@ -1,6 +1,10 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T02_Upload_Practice {
@@ -12,8 +16,15 @@ public class T02_Upload_Practice {
         //        1. Go to https://practice.cydeo.com/upload
         Driver.getDriver().get("https://practice.cydeo.com/upload");
         //        2. Find some small file from your computer, and get the path of it.
+        String path = "C:\\Users\\Atıl\\Desktop\\SDET\\Automation\\HTML\\audi.jpg" ;
+
+        WebElement fileUpload =Driver.getDriver().findElement(By.id("file-upload"));
+        BrowserUtils.sleep(2);
+        fileUpload.sendKeys(path);
         //        3. Upload the file.
+        Driver.getDriver().findElement(By.id("file-submit")).click();
         //        4. Assert:
+        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("//*[@id=\"content\"]/div/h3")).isDisplayed());
         //        -File uploaded text is displayed on the page
 
 
