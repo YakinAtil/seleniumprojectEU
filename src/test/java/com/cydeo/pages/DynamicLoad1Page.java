465dca31c0170450aa5c3ae0031d2085d37ffbb8
Atil Yakin <atlykn@gmail.com>
2022-06-08 01:49:03 +0300

    end of Day12

--- src/test/java/com/cydeo/pages/DynamicLoad1Page.java
+++ src/test/java/com/cydeo/pages/DynamicLoad1Page.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoad1Page {
@@ -8,6 +10,22 @@ public class DynamicLoad1Page {
     public DynamicLoad1Page(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    @FindBy(css="div#loading")
+    public WebElement loadingBar;
 
+    @FindBy(xpath = "//button[.='Start']")
+
+    public WebElement startButton;
+
+    @FindBy(css = "#username")
+    public WebElement username ;
+
+    @FindBy(css = "#pwd")
+    public  WebElement password;
+
+    @FindBy(xpath = "//button[.='Submit']")
+    public WebElement submitButton;
+    @FindBy(id="flash")
+    public WebElement invalidPassword;
 
 }
--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/DynamicLoad1PageTest.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/DynamicLoad1PageTest.java
@@ -1,14 +1,40 @@
 package com.cydeo.tests.day12_pom_design_explicit_wait;
 
+import com.cydeo.pages.DynamicLoad1Page;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class DynamicLoad1PageTest {
+      @Test
 
-    //1. Go to https://practice.cydeo.com/dynamic_loading/1
-    //2. Click to start
-    //3. Wait until loading bar disappears
-    //4. Assert username inputbox is displayed
-    //5. Enter username: tomsmith
-    //6. Enter password: incorrectpassword
-    //7. Click to Submit button
-    //8. Assert “Your password is invalid!” text is displayed.
 
+
+    public void test() {
+          Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
+
+        //2. Click to start
+        DynamicLoad1Page dynamicLoad1Page=new DynamicLoad1Page();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+
+    dynamicLoad1Page.startButton.click();
+        //3. Wait until loading bar disappears
+
+        wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loadingBar));
+        //4. Assert username inputbox is displayed
+        Assert.assertTrue(dynamicLoad1Page.username.isDisplayed());
+        //5. Enter username: tomsmith
+        dynamicLoad1Page.username.sendKeys("tomsmith");
+        //6. Enter password: incorrectpassword
+        dynamicLoad1Page.password.sendKeys("incorrectpassword ");
+        //7. Click to Submit button
+        dynamicLoad1Page.submitButton.click();
+        //8. Assert “Your password is invalid!” text is displayed.
+        Assert.assertTrue((dynamicLoad1Page.invalidPassword.isDisplayed()));
+
+        Driver.closeDriver();
+
+    }
 }
