ef356ec892b99561998bc86ed1832c286c4c9864
atil.yakin <atlykn@gmail.com>
2022-06-07 11:53:24 +0300

    day12

--- src/test/java/com/cydeo/pages/LibraryLoginPage.java
+++ src/test/java/com/cydeo/pages/LibraryLoginPage.java
@@ -1,11 +1,22 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LibraryLoginPage {
+    //1- Open a chrome browser
+    //2- Go to: https://library1.cydeo.com
+    //3- Do not enter any information
+    //4- Click to “Sign in” button
+    //5- Verify expected error is displayed:
+    //Expected: This field is required.
+    //
 
     public  void publicLibraryLogin(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    public WebElement inputUserName;
 }
